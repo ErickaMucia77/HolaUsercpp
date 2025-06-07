afa2205941d2e099b3ff8dbe7bde6533f6db7200
ErickaMucia77 <erickamucia@gmail.com>
2025-06-06 21:50:17 -0600

    Update Main.cpp
    Mejore el código

--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main(){
     string user;
+    string estado;
     
     cout<<"Ingrese su nombre "<<endl;
 
@@ -11,6 +12,8 @@ int main(){
     cout<<"BIENVENIDO " << user<<endl;
     if (user != "" || user != " "){
         cout<<"Hola "<<user<<endl;
+        cout<<"¿Cómo estás?"<<endl;
+        cin>>estado;
     }else{
         cout <<"HOLA MUNDO"<<endl;
     }
